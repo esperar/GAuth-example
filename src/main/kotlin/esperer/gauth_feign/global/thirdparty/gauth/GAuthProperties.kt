53168ec14289070ae3adadedaf80251c9a389884
aesperer <piyrw9754@gmail.com>
2023-04-16 22:33:50 +0900

    :art: gauth thirdparty

---
+++ src/main/kotlin/esperer/gauth_feign/global/thirdparty/gauth/GAuthProperties.kt
@@ -0,0 +1,13 @@
+package esperer.gauth_feign.global.thirdparty.gauth
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+
+@ConstructorBinding
+@ConfigurationProperties(prefix = "gauth")
+data class GAuthProperties(
+    val clientId: String,
+    val clientSecret: String,
+    val redirectUri: String
+)
+
