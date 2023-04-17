6de7e1dd94f0e2ec9240a6a2096174d7ecfad2fd
aesperer <piyrw9754@gmail.com>
2023-04-17 10:14:53 +0900

    :fire: delete unuse import

--- src/main/kotlin/esperer/gauth_feign/global/error/handler/GlobalErrorHandler.kt
+++ src/main/kotlin/esperer/gauth_feign/global/error/handler/GlobalErrorHandler.kt
@@ -2,7 +2,6 @@ package esperer.gauth_feign.global.error.handler
 
 import esperer.gauth_feign.global.error.ErrorResponse
 import esperer.gauth_feign.global.error.exception.BasicException
-import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
