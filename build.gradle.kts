23e0042456847a4079fe90ec823efb0f37d05a37
aesperer <piyrw9754@gmail.com>
2023-04-17 11:12:19 +0900

    :sparkles: jwt properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,6 +36,10 @@ dependencies {
 	// annotation processor
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	implementation("com.github.GSM-MSG:GAuth-SDK-Java:v2.0.0")
+
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 }
 
 tasks.withType<KotlinCompile> {
