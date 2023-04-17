df1b75dada09fc5eacb52c3b0854474cf0d709aa
aesperer <piyrw9754@gmail.com>
2023-04-17 11:14:59 +0900

    :sparkles: jwt parser generator

---
+++ src/main/kotlin/esperer/gauth_feign/global/security/jwt/JwtParser.kt
@@ -0,0 +1,55 @@
+package esperer.gauth_feign.global.security.jwt
+
+import esperer.gauth_feign.global.security.auth.AuthDetailsService
+import esperer.gauth_feign.global.security.exception.ExpiredTokenException
+import esperer.gauth_feign.global.security.exception.InvalidTokenException
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.ExpiredJwtException
+import io.jsonwebtoken.JwtException
+import io.jsonwebtoken.Jwts
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
+import org.springframework.stereotype.Component
+import java.security.Key
+import javax.servlet.http.HttpServletRequest
+
+@Component
+class JwtParser(
+    private val authDetailsService: AuthDetailsService,
+    private val jwtProperties: JwtProperties
+) {
+
+    fun authentication(token: String): Authentication {
+        val userDetails = authDetailsService.loadUserByUsername(getTokenSubject(token, jwtProperties.accessSecret))
+        return UsernamePasswordAuthenticationToken(userDetails, "", userDetails.authorities)
+    }
+
+    fun resolveAccessToken(request: HttpServletRequest): String? =
+        request.getHeader("Authorization")
+            .let { it ?: return null }
+            .let { if (it.startsWith(JwtProperties.TOKEN_PREFIX)) it.replace(JwtProperties.TOKEN_PREFIX, "") else null }
+
+    fun resolveRefreshToken(refreshToken: String): String? =
+        if (refreshToken.startsWith(JwtProperties.TOKEN_PREFIX)) refreshToken.replace(
+            JwtProperties.TOKEN_PREFIX,
+            ""
+        ) else null
+
+
+    private fun getTokenSubject(token: String, secret: Key): String =
+        getTokenBody(token, secret).subject
+
+    private fun getTokenBody(token: String, secret: Key): Claims {
+        return try {
+            Jwts.parserBuilder()
+                .setSigningKey(secret)
+                .build()
+                .parseClaimsJws(token)
+                .body
+        } catch (e: ExpiredJwtException) {
+            throw ExpiredTokenException()
+        } catch (e: JwtException) {
+            throw InvalidTokenException()
+        }
+    }
+}
